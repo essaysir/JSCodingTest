82159a3291238a24a52bacc231e3adbaded8b1a1
Sonjusun <sonjs7554@naver.com>
2023-07-25 22:44:23 +0900

    2023-07-25 코딩테스트 아나그램, 스택 문제 풀이

--- Section5/7_Anagram.js
+++ Section5/7_Anagram.js
@@ -3,8 +3,8 @@ function solution(str1 ,str2){
     let map1 = new Map();
     let map2 = new Map(); 
     for ( let x of str1 ){
-        if (!map1.has(x)) map1.set(x,1);
-        else map1.set(x, (map1.get(x)+1));
+        if (!map1.has(x)) map1.set(x,1)
+        else map1.set(x, (map1.get(x)+1))
     }
     for ( let x of str2 ){
         if (!map2.has(x)) map2.set(x,1);
@@ -16,6 +16,20 @@ function solution(str1 ,str2){
     }
     return answer; 
 }
+function solution_teach(str1,str2){
+    let answer='YES';
+    let sH = new Map();
+    for (let x of str1){
+        if ( sH.has(x)) sH.set(x,sH.get(x)+1)
+        else sH.set(x,1);
+    }
+    for (let x of str2){
+        if(!sH.has(x) || sH.get(x)===0) return "NO";
+        sH.set(x,sH.get(x)-1);
+    }
+
+    return answer ;
+}
 let str1 = 'AbaAeCe';
 let str2 = 'baeeACA';
 let str3 = 'abaCC';
@@ -23,3 +37,5 @@ let str4 = 'Caaab';
 
 console.log( solution(str1,str2));
 console.log( solution(str3,str4));
+console.log( solution_teach(str1,str2));
+console.log( solution_teach(str3,str4));
